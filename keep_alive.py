f4e744612e0caf8b2bc2872dd770d35635bc6f79
MrrLime <96435846+MrrLime@users.noreply.github.com>
2022-02-05 18:30:20 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask('__name__')
 
 @app.route('/')
 def home():
-    return "Hello. I am alive!"
+    return """<a href="https://discordapp.com/oauth2/authorize?client_id=923321635971412040&permissions=4294967287&scope=bot%20applications.commands">Invite Bot"""
 
 def run():
   app.run(host='0.0.0.0',port=8080)
